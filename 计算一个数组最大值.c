1410a5ad906093faa28f3ed0598fd414635a2383
Zan-qaq <112268006+Zan-qaq@users.noreply.github.com>
2022-08-30 21:29:42 +0800

    Update 计算一个数组最大值.c

--- 计算一个数组最大值.c
+++ 计算一个数组最大值.c
@@ -6,7 +6,7 @@ int main()
 	int i = 0;
 	int max = 0;
 	int arr[] = { 1,2,3,4,5,6,7,8,9 };
-	int sz = sizeof(arr) / sizeof(arr[0]);
+	int sz = sizeof(arr) / sizeof(arr[0]);//计算数组元素个数
 	for (i = 0; i < sz; i++)
 	{
 		if (max < arr[i])
